c4b5c5e79d25c17a560f253d999b5e505f6f02c9
Rana-Saleh44 <ranamhsaleh@gmail.com>
2024-04-25 22:02:16 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -16,39 +16,3 @@ class MealsMenuApp extends StatelessWidget {
     );
   }
 }
-// import 'package:flutter/material.dart';
-// import 'package:task3/screens/home_view.dart';
-
-// import 'screens/categories_screen.dart';
-
-// void main() {
-//   runApp(MealsMenuApp());
-// }
-
-// class MealsMenuApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Meals Menu App',
-//       theme: ThemeData(
-//         primarySwatch: Colors.pink,
-//         hintColor: Colors.amber,
-//         canvasColor: Color.fromRGBO(255, 254, 229, 1),
-//         fontFamily: 'Raleway',
-//         textTheme: ThemeData.light().textTheme.copyWith(
-//               bodyText1: TextStyle(
-//                 color: Color.fromRGBO(20, 51, 51, 1),
-//               ),
-//               bodyText2: TextStyle(
-//                 color: Color.fromRGBO(20, 51, 51, 1),
-//               ),
-//               headline6: TextStyle(
-//                 fontSize: 20,
-//                 fontFamily: 'RobotoCondensed',
-//                 fontWeight: FontWeight.bold,
-//               ),
-//             ),
-//       ),
-
-//   }
-// }
